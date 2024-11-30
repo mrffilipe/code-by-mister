ab40db1509ea5f55dd06eaf83766aaea3a236f76
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-11-30 14:46:10 -0300

    First commit

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+
+import App from "./App.tsx";
+
+// biome-ignore lint/style/noNonNullAssertion: Default config from bootstraper
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
