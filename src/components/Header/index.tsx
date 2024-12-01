98c2ed63c3096ba1d8280a63e78c4a9aa6d17323
Filipe Moreira Ferracioli <mrffilipe@outlook.com>
2024-12-01 00:48:48 -0300

    Add Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,62 @@
 import styles from "./styles.module.css";
 
+import { useState } from "react";
+
+// import LogoImg from "../../assets/img/logo.png";
+
 const Header = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsMenuOpen(!isMenuOpen);
+
+		if (!isMenuOpen) {
+			document.body.style.overflow = "hidden";
+		} else {
+			document.body.style.overflow = "";
+		}
+	};
+
 	return (
 		<header className={styles.header}>
-			<h3>Portifolio</h3>
+			<div className={styles.container}>
+				<div className={styles.logo}>
+					<h2>Portifolio</h2>
+					{/* <img src={LogoImg} alt="Logo" /> */}
+				</div>
+				<button className={styles.menu_button} onClick={toggleMenu}>
+					☰
+				</button>
+				<div
+					className={`${styles.overlay} ${isMenuOpen ? styles.visible : ""}`}
+					onClick={toggleMenu}
+				></div>
+				<nav className={`${styles.nav} ${isMenuOpen ? styles.open : ""}`}>
+					<button className={styles.close_button} onClick={toggleMenu}>
+						X
+					</button>
+					<ul className={styles.menu_list}>
+						<li>
+							<a href="#intro">Início</a>
+						</li>
+						<li>
+							<a href="#about">Sobre</a>
+						</li>
+						<li>
+							<a href="#services">Serviços</a>
+						</li>
+						<li>
+							<a href="#consulting">Consultorias</a>
+						</li>
+						<li>
+							<a href="#testimonial">Depoimentos</a>
+						</li>
+						<li>
+							<a href="#contact">Contato</a>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	);
 };
